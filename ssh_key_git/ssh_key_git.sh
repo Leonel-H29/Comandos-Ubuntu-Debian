540bc22533e13dc59ef74fa38a17c6e0d922ff85
Leonel-H29 <leonelherreradkno@gmail.com>
2023-01-06 22:07:07 -0300

    Agrego script automatico para generar la clave SSH para git

---
+++ ssh_key_git/ssh_key_git.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Creando clave ssh."
+ssh_keygen
+
+read -t 3 -p "Espere unos segundos..."
+
+dir="/home/$USER/.ssh/"
+#echo $dir
+let count=$(ls $dir | grep 'id_rsa' | wc -l)
+#echo $count
+
+if [ $count = 2 ]; then
+	echo -e "\nClaves creadas en $dir\n"
+	read -t 5 -p "Mostrando la clave publica..."
+	echo -e "\n"
+	cat $dir"id_rsa.pub"
+	echo -e "\n\n--Copie esta clave en GitHub|GitLab|Bitbutcket antes de continuar--"
+	
+	echo -e  "\n[Pulse cualquier boton para continuar]:"
+	read x
+	read -t 3 -p "Iniciando agente ssh..."
+	eval $(ssh-agent -s)
+	read -t 3 -p "Agregando clave..."
+	ssh-add $dir"id_rsa"
+	echo -e "\nFINISHED!"
+else
+	echo -e "\aError al crear las claves"
+fi
+
+
+
+
