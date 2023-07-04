1e9fab00ef8b3f61296f8d6b007ea3f744818a4a
Leonel-H29 <leonelherreradkno@gmail.com>
2023-07-04 11:31:12 -0300

    Agrego archivos de instalacion de Docker en Ubuntu y Debian

---
+++ docker_operations/README.md
@@ -0,0 +1,19 @@
+# Repositorio de Scripts de Instalación con Docker
+
+Este repositorio contiene una serie de scripts de instalación diseñados para facilitar la configuración de entornos de desarrollo en diferentes distribuciones de Linux utilizando Docker.
+
+## ¿Qué es Docker?
+
+Docker es una plataforma de virtualización de contenedores que permite empaquetar y distribuir aplicaciones junto con todas sus dependencias en entornos aislados. Los contenedores de Docker son livianos, portátiles y ofrecen una forma consistente de implementar aplicaciones en cualquier entorno.
+
+Al utilizar Docker, puedes crear contenedores que contengan todas las dependencias necesarias para ejecutar una aplicación o conjunto de servicios. Estos contenedores son autónomos y se pueden implementar de manera rápida y sencilla en diferentes sistemas operativos y distribuciones, sin preocuparte por las diferencias en la configuración del entorno subyacente.
+
+
+
+## Contribuciones
+
+Si deseas contribuir a este repositorio, ya sea agregando nuevos scripts de instalación o mejorando los existentes, ¡eres bienvenido! Siempre estamos buscando formas de mejorar y ampliar la colección de scripts para adaptarnos a diferentes necesidades y distribuciones.
+
+Si tienes alguna sugerencia, problema o contribución, no dudes en crear un issue o enviar una solicitud de extracción. Agradecemos cualquier ayuda y aportación para hacer que este repositorio sea aún más útil y versátil.
+
+Esperamos que encuentres útiles los scripts de instalación proporcionados y que Docker simplifique la configuración de tu entorno de desarrollo. ¡Disfruta de tu experiencia de desarrollo con Docker y distribuciones Linux!
--- docker_operations/install_on_debian11.sh
+++ docker_operations/install_on_debian11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-######COMANDOS PARA INSTALAR DOCKER EN UBUNTU 20.04 LTS#####
+######COMANDOS PARA INSTALAR DOCKER EN DEBIAN 11#####
 
 
 #Este comando actualiza la lista de paquetes disponibles en los repositorios configurados en el sistema.
@@ -12,14 +12,14 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
 
-#Este comando descarga la clave de firma GPG desde https://download.docker.com/linux/ubuntu/gpg y luego la agrega 
+#Este comando descarga la clave de firma GPG desde https://download.docker.com/linux/debian/gpg y luego la agrega 
 #al almacén de claves del sistema usando apt-key add -.
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 
-#Este comando agrega el repositorio de Docker al sistema. El argumento "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
-#especifica la ubicación del repositorio y utiliza la variable $(lsb_release -cs) para obtener el nombre del código de la versión de Ubuntu actual.
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#Este comando agrega el repositorio de Docker al sistema. El argumento "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 
+#especifica la ubicación del repositorio y utiliza la variable $(lsb_release -cs) para obtener el nombre del código de la versión de Debian actual.
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 
 
@@ -28,14 +28,13 @@ sudo apt-get update
 
 
 
-#Este comando instala el paquete docker-ce y docker-compose, que es la versión de Docker Community Edition (CE) para Ubuntu. 
+#Este comando instala el paquete docker-ce y docker-compose, que es la versión de Docker Community Edition (CE) para Debian. 
 #La opción -y se utiliza para confirmar automáticamente cualquier pregunta de confirmación durante la instalación
 sudo apt-get install docker-ce -y
 sudo apt-get install docker-compose -y
 
 #Este comando habilita el servicio de Docker para que se inicie automáticamente en el arranque del sistema.
 sudo systemctl enable docker
-
 
 
 #Este comando agrega al usuario actual 
